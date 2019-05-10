528068dec229cc5dbf5e7c1f2cade28532d065fa
Spencer.Wang <chuanjen.wang@gmail.com>
2019-05-10 10:30:01 +0800

    LeetCode 283

---
+++ Array/283-moveZero/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+var moveZeroes = function(nums) {
+    let i = 0;
+    let counter = 0;
+    nums.push(-1);
+
+    while (i < nums.length ) {
+        if (nums[i] === -1) break;
+        if (nums[i] === 0) {
+            nums.splice(i, 1);
+            nums.push(0);
+            counter++;
+        } else {
+            i++;
+        }
+    }
+    nums.splice(nums.length - 1 - counter, 1);
+
+    return nums;
+};
+
+const a = moveZeroes([0,0,0,1]);
+
+console.log(a);
